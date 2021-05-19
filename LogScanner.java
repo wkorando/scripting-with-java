cd1ad2000574820ecdc9b66fa9ef726a8d99e3d0
William Korando <wkorando@gmail.com>
2021-05-18 23:44:01 -0500

    Updating code based on feedback

--- LogScanner.java
+++ LogScanner.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class LogScanner {
@@ -13,25 +15,28 @@ public class LogScanner {
 				Welcome to log scanner!
 
 				Enter the phrase you want to search for and the directory you want to search in.
+				
+				   Author: Billy Korando
 				""";
 
 		System.out.println(welcomeMessage);
 
 		System.out.print("Term to search for: ");
-		var searchTerm = new BufferedReader(new InputStreamReader(System.in)).readLine();
+		String searchTerm = new BufferedReader(new InputStreamReader(System.in)).readLine();
 		System.out.print("Directory to search in: ");
-		var rootDirectory = new BufferedReader(new InputStreamReader(System.in)).readLine();
-		var fileType = selectFileType();
+		String rootDirectory = new BufferedReader(new InputStreamReader(System.in)).readLine();
+		String fileType = selectFileType();
 
-		var paths = Files.walk(Paths.get(rootDirectory));
-		var logFiles = paths.filter(p -> p.toFile().isFile()).filter(p -> p.toString().endsWith(fileType)).toList();
+		Stream<Path> paths = Files.walk(Paths.get(rootDirectory));
+		List<Path> logFiles = paths.filter(p -> p.toFile().isFile() && p.toString().endsWith(fileType)).toList();
+		
 		paths.close();
 
 		record LogStatement(String fileName, String logMessage) {
 		}
 
-		var logStatements = new ArrayList<>();
-		for (var p : logFiles) {
+		var logStatements = new ArrayList<LogStatement>();
+		for (Path p : logFiles) {
 			String fileContents = Files.readString(p);
 			Stream.of(fileContents.split("\n")).filter(l -> l.contains(searchTerm))
 					.forEach(l -> logStatements.add(new LogStatement(p.getFileName().toString(), l)));
@@ -41,7 +46,7 @@ public class LogScanner {
 	}
 
 	private static String selectFileType() throws IOException {
-		String fileMessage = """
+		var fileMessage = """
 				Select the type of file you'd like to search:
 				1. .log
 				2. .txt
